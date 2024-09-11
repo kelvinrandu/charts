3e1703a917d5316f9ff8c9bb2c7d20683d51bc1e
randukelvin <randukelvin@gmail.com>
2024-09-11 21:53:42 +0300

    added charts

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -210,6 +210,45 @@ import { Chart } from "react-google-charts";
     risingColor: { strokeWidth: 0, fill: "#0f9d58" }, // green
   },
 };
+export const data8 = [
+  [
+    { type: "date", label: "Day" },
+    "Average temperature",
+    "Average hours of daylight",
+  ],
+  [new Date(2014, 0), -0.5, 5.7],
+  [new Date(2014, 1), 0.4, 8.7],
+  [new Date(2014, 2), 0.5, 12],
+  [new Date(2014, 3), 2.9, 15.3],
+  [new Date(2014, 4), 6.3, 18.6],
+  [new Date(2014, 5), 9, 20.9],
+  [new Date(2014, 6), 10.6, 19.8],
+  [new Date(2014, 7), 10.3, 16.6],
+  [new Date(2014, 8), 7.4, 13.3],
+  [new Date(2014, 9), 4.4, 9.9],
+  [new Date(2014, 10), 1.1, 6.6],
+  [new Date(2014, 11), -0.2, 4.5],
+];
+
+export const options8 = {
+  chart: {
+    title: "Average Temperatures and Daylight in Iceland Throughout the Year",
+  },
+  width: 900,
+  height: 500,
+  series: {
+    // Gives each series an axis name that matches the Y-axis below.
+    0: { axis: "Temps" },
+    1: { axis: "Daylight" },
+  },
+  axes: {
+    // Adds labels to each axis; they don't have to match the axis names.
+    y: {
+      Temps: { label: "Temps (Celsius)" },
+      Daylight: { label: "Daylight" },
+    },
+  },
+};
 
 export default function Home() {
   return (
@@ -243,13 +282,7 @@ export default function Home() {
       data={data3}
       options={options3}
     />
-        <Chart
-      chartType="ScatterChart"
-      width="80%"
-      height="400px"
-      data={data4}
-      options={options4}
-    />
+
         <Chart
       chartType="WordTree"
       width="100%"
@@ -270,6 +303,13 @@ export default function Home() {
       height="400px"
       data={data7}
       options={options7}
+    />
+        <Chart
+      chartType="Line"
+      width="100%"
+      height="400px"
+      data={data8}
+      options={options8}
     />
     </div>
   );
