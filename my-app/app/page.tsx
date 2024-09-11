c956ff3dfaab2a6bc37cc80cbd681071758f0cb0
randukelvin <randukelvin@gmail.com>
2024-09-11 20:39:17 +0300

    added nextsj

---
+++ my-app/app/page.tsx
@@ -0,0 +1,23 @@
+
+'use client'
+
+
+import * as React from 'react';
+
+const uData = [4000, 3000, 2000, 2780, 1890, 2390, 3490];
+const xLabels = [
+  'Page A',
+  'Page B',
+  'Page C',
+  'Page D',
+  'Page E',
+  'Page F',
+  'Page G',
+];
+export default function Home() {
+  return (
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+charts
+    </div>
+  );
+}
